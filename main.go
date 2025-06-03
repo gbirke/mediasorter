27325fb001b215f33584b4b470186985e101b035
Gabriel Birke <gb@birke-software.de>
2025-06-03 22:28:28 +0200

    Start refactoring into a more OOP-style
    `processFile` is becoming too long and has too many option branches and
    error checks that would only need to be done once and could be handled
    with a strategy/command pattern.
    
    This commit outlines the new structure, interfaces and classes, but
    probably breaks the existing code.

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"log/slog"
 	"os"
 	"path/filepath"
 	"text/template"
@@ -13,8 +14,8 @@ import (
 )
 
 // TODO read template from file, explain purpose of whitespace trimming (allows for complex templates with logic)
-// var pathTemplate = "{{- .Artist -}}/{{- .Album -}}/{{- .Track -}} {{- .Title -}}.{{- .Ext -}}"
-var pathTemplate = "{{- .Artist -}}/{{- .Album -}}/{{- .Title -}}"
+// var defaultPathTemplate = "{{- .Artist -}}/{{- .Album -}}/{{- .Track -}} {{- .Title -}}.{{- .Ext -}}"
+var defaultPathTemplate = "{{- .Artist -}}/{{- .Album -}}/{{- .Title -}}"
 
 type Metadata struct {
 	Title    string
@@ -37,37 +38,46 @@ type Options struct {
 	DryRun   bool
 }
 
-// TODO return processing result errors that indicate skipped files
-func processFile(srcPath string, opts *Options) error {
+type MetaDataReader struct {
+	logger *slog.Logger
+}
+
+type NotAMediaFileError struct {
+	srcPath string
+}
+
+func (m *NotAMediaFileError) Error() string {
+	return fmt.Sprintf("'%s' is probably not a media file than can be parsed", m.srcPath)
+}
+
+func (m *MetaDataReader) ReadMetadata(srcPath string) (*Metadata, error) {
 	// read metadata from file
 	f, err := os.Open(srcPath)
 	if err != nil {
-		return fmt.Errorf("error opening file %s: %v", srcPath, err)
+		return nil, fmt.Errorf("error opening file %s: %v", srcPath, err)
 	}
 	defer f.Close()
 
-	// Try to identify the file type to avoid seek errors later
+	// Try to identify the file type, to avoid seek errors in tag.ReadFrom
 	// The files might not be audio files but other metadata files or images
 	_, _, err = tag.Identify(f)
 	if err != nil {
-		// TODO return skip error instead
-		return nil
+		return nil, &NotAMediaFileError{srcPath}
 	}
 
 	// Use github.com/dhowden/tag for reading audio metadata
 	rawMetadata, err := tag.ReadFrom(f)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	fmt.Printf("Metadata for file %s:\n", srcPath)
-	fmt.Printf("Raw: %+v\n", rawMetadata)
+	m.logger.Debug("Metadata for file", "file", srcPath, "rawMetadata", slog.AnyValue(rawMetadata))
 
 	track, _ := rawMetadata.Track()
 	disc, _ := rawMetadata.Disc()
 
 	// TODO clean up metadata - remove newlines, slashes, colons and tabs, to avoid problems with file names
-	m := Metadata{
+	metadata := &Metadata{
 		Title:    rawMetadata.Title(),
 		Artist:   rawMetadata.Artist(),
 		Album:    rawMetadata.Album(),
@@ -79,12 +89,62 @@ func processFile(srcPath string, opts *Options) error {
 		Disc:     disc,
 	}
 
-	// debug print metadata
-	fmt.Printf("%v\n", m)
+	m.logger.Debug("Read Metadata", "metadata", slog.AnyValue(metadata))
+	return metadata, nil
+}
+
+type OverrideChecker interface {
+	DestinationFileExists(destPath string) bool
+}
+
+type NoOverrideChecker struct {
+}
+
+func (n *NoOverrideChecker) DestinationFileExists(destPath string) bool {
+	return false
+}
+
+// TODO add checker implementations for
+// - chaining,
+// - Memory-only (using map[string]struct{} for keeping file names)
+// - filesystem check
+
+// TODO extract implementations from processFile:
+// - OutputProcessor (gets Memory-only OverrideChecker as dependency when doing a dry run, otherwise NoOverrideChecker)
+// - DestinationCreationProcessor
+// - CopyProcessor (gets OverrideChecker as dependency)
+// - MoveProcessor (gets OverrideChecker as dependency)
+type FileProcessor interface {
+	ProcessFile(srcPath string, destPath string)
+}
+
+// TODO slowly migrate processFile (and the loop from the main func) into the MediaSorter
+// Build the list of file processors in main, according to CLI options
+type MediaSorter struct {
+	destDir        string
+	pathTemplate   *template.Template
+	metadataReader *MetaDataReader
+	fileProcessors []FileProcessor
+}
+
+// TODO return processing result errors that indicate skipped files
+func processFile(srcPath string, opts *Options) error {
+	reader := &MetaDataReader{slog.Default()}
+	m, err := reader.ReadMetadata(srcPath)
+
+	if err != nil {
+		re, ok := err.(*NotAMediaFileError)
+		if ok {
+			fmt.Println(re.Error())
+			// TODO return result with error msg (skipped) instead of printing, leaving the printing to the output
+			return nil
+		}
+		return err
+	}
 
 	// render metadata as text
 	// TODO move template parsing to main and pass template to processFile
-	templ, err := template.New("path").Parse(pathTemplate)
+	templ, err := template.New("path").Parse(defaultPathTemplate)
 	if err != nil {
 		return fmt.Errorf("error parsing template: %v", err)
 	}
@@ -102,6 +162,7 @@ func processFile(srcPath string, opts *Options) error {
 
 	// check if path exists, skip file if override is not set
 	if !opts.Override {
+		// TODO if dry-run is set, use a different check, e.g. hash map
 		if _, err := os.Stat(newFileName); err == nil {
 			// TODO return skip error instead
 			fmt.Printf("File %s already exists, skipping\n", newFileName)
@@ -131,6 +192,11 @@ func processFile(srcPath string, opts *Options) error {
 			return fmt.Errorf("error creating file %s: %v", newFileName, err)
 		}
 		defer destFile.Close()
+		f, err := os.Open(srcPath)
+		if err != nil {
+			return fmt.Errorf("error opening file %s: %v", srcPath, err)
+		}
+		defer f.Close()
 		_, err = io.Copy(destFile, f)
 		if err != nil {
 			return fmt.Errorf("error copying file %s to %s: %v", srcPath, newFileName, err)
@@ -147,6 +213,7 @@ func main() {
 	dryRun := flag.Bool("dry-run", false, "Do not move/copy files, just print the new file names")
 	// TODO add flag for template and/or template file
 	// TODO add flag for verbosity and help
+	// TODO add flag for progress bar (mutually exclusive with verbosity)
 
 	flag.Parse()
 	args := flag.Args()
@@ -158,6 +225,9 @@ func main() {
 	srcDir := args[0]
 	destDir := args[1]
 
+	// TODO use options to build the chained override checkers and the list of file processors
+	//      Drop options at the end
+
 	// Create options struct with inline initialization
 	opts := &Options{
 		DestDir:  destDir,
@@ -166,15 +236,18 @@ func main() {
 		DryRun:   *dryRun,
 	}
 
+	// TODO move into MediaSorter class
 	// iterate over all files in source directory
 	err := filepath.Walk(srcDir, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
+		// TODO recusion through subdirectories?
 		if info.IsDir() {
 			return nil
 		}
 
+		// TODO pass to goroutine for parallel processing of files?
 		err = processFile(path, opts)
 
 		if err == tag.ErrNoTagsFound {
@@ -191,4 +264,3 @@ func main() {
 		os.Exit(1)
 	}
 }
-
